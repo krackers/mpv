52946315a2d61c6a37d0fac38c6b42eee2424e4f
krackers <krackers@users.noreply.github.com>
2024-02-26 03:25:09 +0000

    options: Split "window-scale" runtime property
    Create "initial-window-scale" for the scale used when reconfiguring
    VO, and "window-scale" to control the current window scale.

--- options/options.h
+++ options/options.h
@@ -35,7 +35,7 @@ typedef struct mp_vo_opts {
     struct m_geometry autofit;
     struct m_geometry autofit_larger;
     struct m_geometry autofit_smaller;
-    double window_scale;
+    double initial_window_scale;
 
     int keepaspect;
     int keepaspect_window;
