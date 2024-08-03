9879ef91c5df4243e8b7fa97818b4b391323ebc2
krackers <krackers@users.noreply.github.com>
2024-08-03 03:03:48 +0000

    sub: Remove scale-to-window option.
    Libass already does the sensible thing and scales
    fonts with respect to the video size if ``--sub-use-margins`` is disabled
    or with respect to the window-fit size if margins is enabled.
    (The window-fit size is not necessarily the window size in the case aspect
    ratio of video differs from that of the frame).

--- options/options.h
+++ options/options.h
@@ -80,8 +80,6 @@ struct mp_subtitle_opts {
     int stretch_image_subs;
     int image_subs_video_res;
     int sub_fix_timing;
-    int sub_scale_with_window;
-    int ass_scale_with_window;
     struct osd_style_opts *sub_style;
     float sub_scale;
     float sub_gauss;
